a5501b40477c14753a576c11eefdac4320c2d008
yasmine <yasmine_1304@yahoo.com>
2021-05-09 17:45:10 +0200

    health

---
+++ dentalclinichousegoals.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+	<html lang="en">
+
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- Link to the google font  -->
+		<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500&display=swap" rel="stylesheet">
+		<!-- Link to the CSS file  -->
+		<link rel="stylesheet" href="style.css">
+		<title>Dental clinic house goals </title>
+	</head>
+    <body>
+    <?php
+   include("header.php");?>
+        <div class="view-container" style="text-align:center;margin-top: 100px;">
+        <div class="row" style="width: 50%;margin:0 auto;">
+    <div class="column">
+      <img src="ehr.svg" alt="Create Electronic Health Records" style="width: 30%">
+      <h3>Create Electronic Health Records</h3>
+    
+    </div>
+    <div class="column">
+      <img src="calendar.svg" alt="calendar" style="width:30%">
+      <h3>Schedule Appointments</h3>
+    </div>
+    <div class="column">
+      <img src="reminders.svg" alt="Reminder" style="width:30%">
+      <h3>Send Automated Reminders</h3>
+    </div>
+    <br>
+  </div>
+  <div class="row" style="width: 50%;margin:0 auto;">
+  <div class="column">
+      <img src="treatment-plans.svg" alt="treatment" style="width:30%">
+      <h3>Build Treatment Plans</h3>
+    </div>
+    <div class="column">
+      <img src="inventory.svg" alt="inventory" style="width:30%">
+      <h3>Manage Your Inventory</h3>
+    </div>
+    <div class="column">
+      <img src="revenue.svg" alt="Revenue" style="width:30%">
+      <h3>Track Your Revenue</h3>
+    </div>
+  </div>
+        </div>
+
+</body>
+  </html>
