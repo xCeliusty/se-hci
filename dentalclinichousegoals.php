3d81ba4d5cd096fcf530a05f16d468a2baca6d06
yasmine <yasmine_1304@yahoo.com>
2021-05-09 19:36:22 +0200

    edit pages

--- dentalclinichousegoals.php
+++ dentalclinichousegoals.php
@@ -7,43 +7,43 @@
 		<!-- Link to the google font  -->
 		<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500&display=swap" rel="stylesheet">
 		<!-- Link to the CSS file  -->
-		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="layouts/css/style.css">
 		<title>Dental clinic house goals </title>
 	</head>
     <body>
     <?php
-   include("header.php");?>
+   include("includes/header.php");?>
         <div class="view-container" style="text-align:center;margin-top: 100px;">
         <div class="row" style="width: 50%;margin:0 auto;">
-    <div class="column">
-      <img src="ehr.svg" alt="Create Electronic Health Records" style="width: 30%">
-      <h3>Create Electronic Health Records</h3>
-    
-    </div>
-    <div class="column">
-      <img src="calendar.svg" alt="calendar" style="width:30%">
-      <h3>Schedule Appointments</h3>
-    </div>
-    <div class="column">
-      <img src="reminders.svg" alt="Reminder" style="width:30%">
-      <h3>Send Automated Reminders</h3>
-    </div>
-    <br>
-  </div>
-  <div class="row" style="width: 50%;margin:0 auto;">
-  <div class="column">
-      <img src="treatment-plans.svg" alt="treatment" style="width:30%">
-      <h3>Build Treatment Plans</h3>
-    </div>
-    <div class="column">
-      <img src="inventory.svg" alt="inventory" style="width:30%">
-      <h3>Manage Your Inventory</h3>
-    </div>
-    <div class="column">
-      <img src="revenue.svg" alt="Revenue" style="width:30%">
-      <h3>Track Your Revenue</h3>
-    </div>
-  </div>
+          <div class="column">
+            <img src="layouts/imgs/ehr.svg" alt="Create Electronic Health Records" style="width: 30%">
+            <h3>Create Electronic Health Records</h3>
+          
+          </div>
+          <div class="column">
+            <img src="layouts/imgs/calendar.svg" alt="calendar" style="width:30%">
+            <h3>Schedule Appointments</h3>
+          </div>
+          <div class="column">
+            <img src="layouts/imgs/reminders.svg" alt="Reminder" style="width:30%">
+            <h3>Send Automated Reminders</h3>
+          </div>
+          <br>
+        </div>
+        <div class="row" style="width: 50%;margin:0 auto;">
+        <div class="column">
+            <img src="layouts/imgs/treatment-plans.svg" alt="treatment" style="width:30%">
+            <h3>Build Treatment Plans</h3>
+          </div>
+          <div class="column">
+            <img src="layouts/imgs/inventory.svg" alt="inventory" style="width:30%">
+            <h3>Manage Your Inventory</h3>
+          </div>
+          <div class="column">
+            <img src="layouts/imgs/revenue.svg" alt="Revenue" style="width:30%">
+            <h3>Track Your Revenue</h3>
+          </div>
+        </div>
         </div>
 
 </body>
